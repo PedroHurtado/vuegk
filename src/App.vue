5f9d2447ab26156dc0acac7aff178b6c3d2d4473
Pedro Hurtado <pedrohurt@gmail.com>
2025-02-26 09:40:19 +0100

    slots

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import List from './components/carrito/List.vue';
 import Carrito from './components/carrito/Carrito.vue';
 import { PubSub } from './services/pubsub';
@@ -8,20 +8,32 @@ import Input from './components/input/Input.vue';
 import { useInput } from './components/input/useInput';
 import { provide } from 'vue';
 import Layout from './components/pages/Layout.vue';
-
+import Button from './components/slots/Button.vue';
+import Dialog from './components/slots/Dialog.vue';
 const pubsub = new PubSub<Pizza>()
 
 const phone = reactive(useInput());
 
 provide('carrito', pubsub)
-
+const dialog = ref();
+function handlerclick(ev:Event){
+    dialog.value.open();
+}
 </script>
 
 <template> 
+    
+    <Button @click="handlerclick($event)">Abrir dialog</Button>
+    <Dialog ref="dialog">
+        <h1>Dialog</h1>
+        <p>Contenido del dialog</p>
+    </Dialog>
+    <!--
     <Layout/>
     <Input v-model="phone.value" label="Teléfono" id="phone" />
     {{ phone.value }}
     <Carrito/>    
     <List/>    
+    -->
 </template>
 
---
+++ src/components/slots/Dialog.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+const dialog = ref<HTMLDialogElement | null>(null);
+
+const open = () => {
+    dialog.value?.showModal();
+};
+const close = (ev:Event)=>{
+    ev.stopPropagation();
+    dialog.value?.close();
+}
+defineExpose({ open });
+
+</script>
+<template>
+    <dialog ref="dialog">
+        <form method="dialog">
+            <section>
+                <slot></slot>
+            </section>
+            <menu>
+                <button @click="close($event)">Cerrar</button>
+                <button type="submit">Confirm</button>
+            </menu>
+        </form>
+    </dialog>
+
+</template>
